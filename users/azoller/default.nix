07a4dc4c6d85a2acfaf407b361fecc0c8465e823
Alexander Zoller <personal@alexanderzoller.com>
2025-09-24 16:50:10 -0500

    add user back to podman group

--- users/azoller/default.nix
+++ users/azoller/default.nix
@@ -6,7 +6,7 @@
     #linger = true;
     autoSubUidGidRange = true;
     home = "/home/azoller";
-    extraGroups = [ "wheel" "networkmanager" "video" ];
+    extraGroups = [ "wheel" "networkmanager" "video" "podman" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIukVfBl4xdLkVYoBsAfsrUQ7aG5qFiObDZbK8j6UGZj azoller@pc-linux"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbbugMH2sE7ym1qtLBHY9rHoSgboe/rOFmQSL9zYzVS alexz@laptop"
