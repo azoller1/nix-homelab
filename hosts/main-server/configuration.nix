86c8ef8890ccc138b7acb8ce00c1ac705f8f61ce
Alexander Zoller <personal@alexanderzoller.com>
2025-10-02 15:37:17 -0500

    import into main-server for romm

--- hosts/main-server/configuration.nix
+++ hosts/main-server/configuration.nix
@@ -4,10 +4,13 @@
   ## Imports
   # Hardware/Disks
   # Users
+  # Modules/Containers
   imports = [
     ./disk-config.nix
     ../../users/azoller/default.nix
+    ../../modules/containers/romm/default.nix
   ];
+
   facter.reportPath = ./facter.json;
 
   # Root user key, all hosts
@@ -254,36 +257,10 @@
   };
 
   ## Containers
+
+  # Imports
+
   virtualisation.oci-containers.containers = {
-
-    romm = {
-      image = "ghcr.io/rommapp/romm:4.3.0";
-      autoStart = true;
-      networks = ["romm"];
-      hostname = "romm";
-
-      volumes = [
-        "romm_data:/romm/resources"
-        "romm_redis_data:/redis-data"
-        "/mnt/data/romm/library:/romm/library"
-        "/mnt/data/romm/assets:/romm/assets"
-      ];
-
-      environmentFiles = [
-        /home/azoller/nix-homelab/hosts/main-server/.env.secret.romm
-      ];
-
-      labels = {
-        "traefik.enable" = "true";
-        "traefik.http.services.romm.loadbalancer.server.port" = "8080";
-        "traefik.http.routers.romm.rule" = "Host(`romm.azollerstuff.xyz`)";
-        "traefik.http.routers.romm.entrypoints" = "https";
-        "traefik.http.routers.romm.tls" = "true";
-        "traefik.http.routers.romm.tls.certresolver" = "le";
-        "traefik.http.routers.romm.tls.domains[0].main" = "*.azollerstuff.xyz";
-        "traefik.http.routers.romm.middlewares" = "secheader@file";
-      };
-    };
 
     beszel = {
       image = "ghcr.io/henrygd/beszel/beszel:0.12.7";
