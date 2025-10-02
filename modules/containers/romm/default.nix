cd163fe362956a2d676a5b8c2c70a8b180209759
Alexander Zoller <personal@alexanderzoller.com>
2025-10-02 15:28:46 -0500

    add sops and secrets folder, testing romm

---
+++ modules/containers/romm/default.nix
@@ -0,0 +1,38 @@
+{
+
+    sops.age.sshKeyPaths = [ "/home/azoller/.ssh/id_ed25519" ];
+    sops.secrets.romm_env = {
+      sopsFile = "../../../secrets/romm_secret.env";
+      format = "dotenv";
+    };
+
+    virtualisation.oci-containers.containers."romm" = {
+
+        image = "ghcr.io/rommapp/romm:4.3.0";
+        autoStart = true;
+        networks = ["romm"];
+        hostname = "romm";
+
+        volumes = [
+            "romm_data:/romm/resources"
+            "romm_redis_data:/redis-data"
+            "/mnt/data/romm/library:/romm/library"
+            "/mnt/data/romm/assets:/romm/assets"
+        ];
+
+        environmentFiles = [
+            /run/secrets/romm_env
+        ];
+
+        labels = {
+            "traefik.enable" = "true";
+            "traefik.http.services.romm.loadbalancer.server.port" = "8080";
+            "traefik.http.routers.romm.rule" = "Host(`romm.azollerstuff.xyz`)";
+            "traefik.http.routers.romm.entrypoints" = "https";
+            "traefik.http.routers.romm.tls" = "true";
+            "traefik.http.routers.romm.tls.certresolver" = "le";
+            "traefik.http.routers.romm.tls.domains[0].main" = "*.azollerstuff.xyz";
+            "traefik.http.routers.romm.middlewares" = "secheader@file";
+        };
+    };
+}
