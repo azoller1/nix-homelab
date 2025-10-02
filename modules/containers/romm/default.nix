a720ade378bdcf038c3dd7fdcdfa05ae2d65719b
Alexander Zoller <personal@alexanderzoller.com>
2025-10-02 16:40:35 -0500

    remove from module

--- modules/containers/romm/default.nix
+++ modules/containers/romm/default.nix
@@ -1,11 +1,4 @@
 {
-
-    sops.age.sshKeyPaths = [ "/home/azoller/.ssh/id_ed25519" ];
-    sops.secrets.romm_env = {
-      sopsFile = "/home/azoller/nix-homelab/secrets/romm_secret.env";
-      format = "dotenv";
-    };
-
     virtualisation.oci-containers.containers."romm" = {
 
         image = "ghcr.io/rommapp/romm:4.3.0";
