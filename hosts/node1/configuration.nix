9637ef79007a474a02ac425402cdc8cd2eadc51b
Alexander Zoller <personal@alexanderzoller.com>
2025-10-14 14:05:27 -0500

    update node1 beszel

--- hosts/node1/configuration.nix
+++ hosts/node1/configuration.nix
@@ -156,6 +156,55 @@
 
   virtualisation.oci-containers.containers = {
 
+    socket-proxy-beszel = {
+      image = "lscr.io/linuxserver/socket-proxy:3.2.4";
+      autoStart = true;
+      networks = ["beszel"];
+      hostname = "socket-proxy-beszel";
+
+      volumes = [
+        "/var/run/docker.sock:/var/run/docker.sock:ro"
+      ];
+
+      environment = {
+        CONTAINERS = "1";
+        LOG_LEVEL = "info";
+        TZ = "America/Chicago";
+      };
+
+      extraOptions = [
+        "--tmpfs=/run"
+        "--read-only"
+        "--memory=64m"
+        "--cap-drop=ALL"
+        "--security-opt=no-new-privileges"
+      ];
+    };
+
+    beszel-agent = {
+      image = "ghcr.io/henrygd/beszel/beszel-agent:0.12.7";
+      autoStart = true;
+      ports = ["45876:45876"];
+      networks = ["beszel"];
+      hostname = "beszel-agent";
+
+      environment = {
+        LISTEN = "45876";
+        HUB_URL = "https://stats.azollerstuff.xyz";
+        DOCKER_HOST = "tcp://socket-proxy-beszel:2375";
+        KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHjnUWIIjbWBWrZhZUiItXjHh1A5wo+8ABvWunvCfTb";
+      };
+
+
+      volumes = [
+        "beszel-agent_data:/var/lib/beszel-agent"
+      ];
+
+      labels = {
+        "traefik.enable" = "false";
+      };
+    };
+
     marknotes = {
       image = "ghcr.io/fccview/rwmarkable:1.5.0";
       autoStart = true;
