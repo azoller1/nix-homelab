9c77f5671a5c17d3fc64087d3905bd5c81aafe3a
Alexander Zoller <personal@alexanderzoller.com>
2025-10-18 23:55:25 -0500

    necesse fixed again

--- modules/containers/necesse/default.nix
+++ modules/containers/necesse/default.nix
@@ -1,9 +1,9 @@
 {
     virtualisation.oci-containers.containers."necesse" = {
 
-        image = "git.azollerstuff.xyz/azoller/necesse:1.0.1";
+        image = "git.azollerstuff.xyz/azoller/necesse:1.0.1-v2";
         autoStart = true;
-        ports = ["14159:14159"];
+        ports = ["14159:14159/udp"];
         networks = ["necesse"];
         hostname = "necesse";
 
