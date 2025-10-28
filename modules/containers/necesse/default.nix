f518e47571b663ae76b00b2e5f643c5b0f95d7d0
Alexander Zoller <personal@alexanderzoller.com>
2025-10-28 16:55:14 -0500

    update necesse

--- modules/containers/necesse/default.nix
+++ modules/containers/necesse/default.nix
@@ -43,7 +43,7 @@
 
     virtualisation.oci-containers.containers."necesse" = {
 
-        image = "git.azollerstuff.xyz/azoller/necesse:1.0.1-v2";
+        image = "git.azollerstuff.xyz/azoller/necesse:1.0.2";
         #autoStart = true;
         ports = ["14159:14159/udp"];
         networks = ["necesse"];
