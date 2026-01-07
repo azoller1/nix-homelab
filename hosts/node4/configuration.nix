e2815e7b2b7e917931b5b6d44247292bb659e197
Alexander Zoller <personal@alexanderzoller.com>
2026-01-07 13:27:49 -0600

    main-server root ssh access

--- hosts/node4/configuration.nix
+++ hosts/node4/configuration.nix
@@ -19,6 +19,7 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIukVfBl4xdLkVYoBsAfsrUQ7aG5qFiObDZbK8j6UGZj azoller@pc-linux"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbbugMH2sE7ym1qtLBHY9rHoSgboe/rOFmQSL9zYzVS alexz@laptop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAdD8uhvY1tggw+U5zOf831nn43Z81atlFh+zuprFtZd azoller@main-server"
   ];
 
   # Bootloader
