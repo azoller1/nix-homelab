7df1ca0d866afc9c9ce3f958efdee71f7ff3f370
Alexander Zoller <personal@alexanderzoller.com>
2026-01-13 22:36:23 -0600

    sftpgo webdav

--- modules/containers/sftpgo/default.nix
+++ modules/containers/sftpgo/default.nix
@@ -13,6 +13,11 @@
             "sftpgo_extra:/var/lib/sftpgo"
         ];
 
+        environment = {
+            SFTPGO_WEBDAVD__BINDINGS__0__PORT = "8181";
+            SFTPGO_WEBDAVD__BINDINGS__0__PREFIX = "/webdav";
+        };
+
         labels = {
             "traefik.enable" = "true";
             "traefik.http.services.sftpgo.loadbalancer.server.port" = "8080";
@@ -23,6 +28,14 @@
             "traefik.http.routers.sftpgo.tls.certresolver" = "le";
             "traefik.http.routers.sftpgo.tls.domains[0].main" = "*.zollerlab.com";
             "traefik.http.routers.sftpgo.middlewares" = "secheader@file";
+            "traefik.http.services.sftpgo-dav.loadbalancer.server.port" = "8181";
+            "traefik.http.routers.sftpgo-dav.service" = "sftpgo-dav";
+            "traefik.http.routers.sftpgo-dav.rule" = "Host(`files.zollerlab.com`) && PathPrefix(`/webdav`)";
+            "traefik.http.routers.sftpgo-dav.entrypoints" = "https";
+            "traefik.http.routers.sftpgo-dav.tls" = "true";
+            "traefik.http.routers.sftpgo-dav.tls.certresolver" = "le";
+            "traefik.http.routers.sftpgo-dav.tls.domains[0].main" = "*.zollerlab.com";
+            "traefik.http.routers.sftpgo-dav.middlewares" = "secheader@file";
         };
     };
 }
