e2390731d605f9a2a93db6e26e8e333cdd03324a
Alexander Zoller <personal@alexanderzoller.com>
2026-01-28 21:26:00 -0600

    forgejo ssh updates

--- modules/containers/forgejo/default.nix
+++ modules/containers/forgejo/default.nix
@@ -5,6 +5,7 @@
 
         image = "codeberg.org/forgejo/forgejo:14.0.1-rootless";
         networks = ["forgejo"];
+        ports = ["2222:2222"];
         hostname = "forgejo";
 
         environment = {
@@ -20,25 +21,15 @@
         ];
 
         labels = {
-            "wud.watch" = "true";
-            "wud.tag.include" = "^[0-9]+.[0-9]+.[0-9]+-rootless$";
-            "wud.link.template" = "https://codeberg.org/forgejo/forgejo/releases";
             "traefik.enable" = "true";
             "traefik.http.routers.forgejo.service" = "forgejo";
             "traefik.http.services.forgejo.loadbalancer.server.port" = "3000";
-            "traefik.http.routers.forgejo.rule" = "Host(`git.azollerstuff.xyz`)";
+            "traefik.http.routers.forgejo.rule" = "Host(`git.zollerlab.com`)";
             "traefik.http.routers.forgejo.entrypoints" = "https";
             "traefik.http.routers.forgejo.tls" = "true";
             "traefik.http.routers.forgejo.tls.certresolver" = "le";
-            "traefik.http.routers.forgejo.tls.domains[0].main" = "*.azollerstuff.xyz";
+            "traefik.http.routers.forgejo.tls.domains[0].main" = "*.zollerlab.com";
             "traefik.http.routers.forgejo.middlewares" = "secheader@file";
-            "traefik.tcp.routers.forgejoSSH.service" = "forgejoSSH";
-            "traefik.tcp.services.forgejoSSH.loadbalancer.server.port" = "2222";
-            "traefik.tcp.routers.forgejoSSH.rule" = "HostSNI(`git.azollerstuff.xyz`)";
-            "traefik.tcp.routers.forgejoSSH.entrypoints" = "https";
-            "traefik.tcp.routers.forgejoSSH.tls" = "true";
-            "traefik.tcp.routers.forgejoSSH.tls.certresolver" = "le";
-            "traefik.tcp.routers.forgejoSSH.tls.domains[0].main" = "*.azollerstuff.xyz";
         };
     };
 }
