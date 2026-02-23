c04ec87735fca4a06345540d57c2855ec0c8d90a
Alexander Zoller <personal@alexanderzoller.com>
2026-02-23 14:12:19 -0600

    traefik geoblock cache

--- modules/containers/traefik/default.nix
+++ modules/containers/traefik/default.nix
@@ -66,6 +66,7 @@
 
         volumes = [
             "/home/azoller/containers/traefik/acme.json:/acme.json"
+            "/home/azoller/containers/traefik/geoblock/ip-cache.db:/ip-cache.db"
             "/home/azoller/containers/traefik/config.yaml:/etc/traefik/traefik.yaml:ro"
             "/home/azoller/containers/traefik/dynamic:/etc/traefik/dynamic/:ro"
         ];
